cc84b058b6cebbc45dbda54fb24981ef18125dcb
dovrannazar <dvrnnzr@gmail.com>
2020-11-18 15:27:19 -0500

    HR update

--- src/main/java/POM/HRElements.java
+++ src/main/java/POM/HRElements.java
@@ -1,4 +1,53 @@
 package POM;
 
-public class HRElements {
+import Utilities.DriverFactory;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HRElements extends ParentClass {
+    public HRElements(){
+        PageFactory.initElements(DriverFactory.getDriver(),this);
+
+    }
+
+    @FindBy(xpath = "//span[text()='Human Resources']")
+    private WebElement HRBtn;
+
+    @FindBy(xpath = "(//span[text()='Setup'])[3]")
+    private WebElement HRSetupBtn;
+
+    @FindBy(xpath = "//span[text()='Employees']")
+    private WebElement employeesBtn;
+
+
+
+    WebElement myElement;
+
+    public void findElementAndClickFunction(String ElementName){
+
+        switch (ElementName) {
+            case "HRBtn":
+                myElement = HRBtn;
+                break;
+
+        }
+        clickFunction(myElement);
+    }
+
+    public void findInputAndSendKeys(String ElementName,String value){
+        switch (ElementName) {
+            case "HRBtn":
+                myElement = HRBtn;
+                break;
+            case "HRSetupBtn":
+                myElement =HRSetupBtn;
+                break;
+            case "employeesBtn":
+                myElement =employeesBtn;
+                break;
+        }
+        sendKeysFunction(myElement,value);
+    }
+
 }
---
+++ src/test/java/FeatureFile/HR.feature
@@ -0,0 +1,7 @@
+Feature: HR testing
+  Scenario: Position Salary
+    Given Navigate to the website
+    And Enter username and password
+    And Click Login button
+    And Go to the HR
+    And Click Position Salary
